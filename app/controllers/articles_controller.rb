37cf68d923d3bd2d31756f7fe42f2b4122eb0e77
delhen <hendardidel@gmail.com>
2022-07-15 23:23:00 +0700

    Learn about create method and its additional front end features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # Strong parameters
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created successfully!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    # render plain: @article.inspect
+  end
 end
