53660aafb2d963db8c6533abaabb7325fb7bdb7a
delhen <hendardidel@gmail.com>
2022-07-16 10:09:49 +0700

    Added show, edit, and delete layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,25 @@ class ArticlesController < ApplicationController
     end
     # render plain: @article.inspect
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article successfully deleted!"
+    redirect_to articles_path
+  end
 end
