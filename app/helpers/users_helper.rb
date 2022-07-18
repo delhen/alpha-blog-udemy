b7c4e89db2aaa439038fa909d720f42fdd3cc200
delhen <hendardidel@gmail.com>
2022-07-18 12:24:36 +0700

    Added user list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options: {size: 80})
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
-    image_src = "https://www.gravatar.com/avatar/#{hash}?s=128"
+    size = options[:size]
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(image_src, alt: user.username, class: "rounded shadow-sm")
   end
 end
