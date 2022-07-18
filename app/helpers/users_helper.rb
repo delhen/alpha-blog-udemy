3f4f6f404364c46bb22f73d19fed5e0928bfa76a
delhen <hendardidel@gmail.com>
2022-07-18 12:05:22 +0700

    Create user profile page, add user helper method, refactor article listing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user)
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=128"
+    image_tag(image_src, alt: user.username, class: "rounded shadow-sm")
+  end
 end
